31f5b8eb6fb603c570e4251d4c69d776922e2b6f
Zeldork <aparisan@udel.edu>
2022-10-20 16:57:22 -0400

    Project

---
+++ PongProject/Assets/Scripts/Tests/UnitTests.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class UnitTests
+{
+    // Test if Player2 can score
+    [Test]
+    public void UnitTestsSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // Test if Player1 can score
+   
+}
