8841279ed7c59a60088d5b624c49f83ecda528f8
Zeldork <aparisan@udel.edu>
2022-11-10 13:26:43 -0500

    Hw 9

---
+++ Week9/Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        // change ball size
+        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+    }
+}
+
---
+++ Week9/Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        // change ball color
+        GetComponent<Renderer>().material.color = new Color(0, 206, 209);
+    }
+}
+
